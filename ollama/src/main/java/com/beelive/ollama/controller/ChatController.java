0e7dcd49fa02a83c7496c1f7cff9f65f464f4df2
raviParihar94 <pariharravi480@gmail.com>
2026-07-26 10:04:01 +0530

    feat: implement ChatMemory API

--- ollama/src/main/java/com/beelive/ollama/controller/ChatController.java
+++ ollama/src/main/java/com/beelive/ollama/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.beelive.ollama.controller;
 
 import com.beelive.ollama.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +25,7 @@ public class ChatController {
     public String chat(@RequestParam("message") String message ){
         String response  = chatClient.
                 prompt()
-                .advisors( new TokenUsageAuditAdvisor())
+                .options(OllamaChatOptions.builder().minP(0.8))
                 .system(
                         """
 You are a professional customer service assistant which helps drafting email responses to improve the productivity of the customer support team

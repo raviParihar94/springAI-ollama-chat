a51fd0d9ca4b3f2076bad08051b9785a577d9b3a
raviParihar94 <pariharravi480@gmail.com>
2026-07-28 21:51:19 +0530

    feat: implemanting RAG and  qdrant VectorDb

--- ollama/src/main/java/com/beelive/ollama/configuration/ChatMemoryChatClientConfig.java
+++ ollama/src/main/java/com/beelive/ollama/configuration/ChatMemoryChatClientConfig.java
@@ -1,11 +1,19 @@
 package com.beelive.ollama.configuration;
 
 import com.beelive.ollama.advisor.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.cache.semantic.SemanticCacheAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.postretrieval.document.DocumentPostProcessor;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +21,35 @@ import java.util.List;
 
 @Configuration
 public class ChatMemoryChatClientConfig {
-    @Bean("chat-memory-chatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
-      Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
 
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory
+            , RetrievalAugmentationAdvisor retrievalAugmentationAdvisor, SemanticCacheAdvisor semanticCacheAdvisor) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
-                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor(), memoryAdvisor))
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor,tokenUsageAdvisor,
+                        retrievalAugmentationAdvisor,semanticCacheAdvisor))
+                .build();
+    }
 
+    @Bean
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore,
+                                                              ChatClient.Builder chatClientBuilder) {
+        return RetrievalAugmentationAdvisor.builder()
+                .queryTransformers(TranslationQueryTransformer.builder()
+                        .chatClientBuilder(chatClientBuilder.clone())
+                        .targetLanguage("english").build())
+                .documentRetriever(VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)
+                        .topK(3).similarityThreshold(0.5).build())
+                .documentPostProcessors()
                 .build();
     }
 
--- ollama/src/test/java/com/beelive/ollama/controller/PromptStuffingControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.beelive.ollama.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PromptStuffingControllerTest {
-
-}
